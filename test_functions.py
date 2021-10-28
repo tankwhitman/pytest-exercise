e4fa306c5868a5bc1a9b8f31f10799ec48bf1165
Joshua <tankwhitman@gmail.com>
2021-10-28 15:11:50 -0500

    Worked on a couple

--- test_functions.py
+++ test_functions.py
@@ -1,5 +1,28 @@
 import pytest
 from functions import *
 
-def test_openFile:
+def test_openFile():
     assert openFile("") == 6
+
+@pytest.mark.parametrize("first, second, equal", [(100,10, 10), (144, 12, 12), ("lol","string", "another string")])
+def test_numbers(first, second, equal):
+    assert numbers(first, second) == equal
+
+@pytest.mark.parametrize("word, answer", [("racecar", True), ("543212345", True), ("randomword", False), (12, False)])
+def test_isPalindrome(word, answer):
+    assert isPalindrome(word) == answer
+
+
+## these arent exactly working...
+def geninputs():
+    inputs = ["64", "8", "81", "9"]
+
+    for item in inputs:
+        yield item
+
+GEN = geninputs()
+
+def test_divide(divider):
+    divider.setattr('builtins.input', lambda _: next(GEN))
+
+    assert divide()
