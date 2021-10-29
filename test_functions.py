fd6cbf282fc33be2a54507713bd4954f3e4d242b
Austin <austin.reed1995@gmail.com>
2021-10-29 16:28:48 -0500

    I made changes josh saw it

--- test_functions.py
+++ test_functions.py
@@ -1,28 +1,46 @@
 import pytest
 from functions import *
 
-def test_openFile():
-    assert openFile("") == 6
 
-@pytest.mark.parametrize("first, second, equal", [(100,10, 10), (144, 12, 12), ("lol","string", "another string")])
+## 1
+def test_openFile(capsys):
+    openFile("testing.txt")
+    captured_stdout, captured_stderr = capsys.readouterr()
+    assert captured_stdout.strip() == "File opened."
+
+
+
+## 2
+@pytest.mark.parametrize("first, second, equal", [(100, 10, 10), (144, 12, 12), ("lol", "string", "another string")])
 def test_numbers(first, second, equal):
     assert numbers(first, second) == equal
 
+
+@pytest.mark.parametrize("Num1, Num2, equal", [("num1", "num2",10)])
+def test_numbers(Num1, Num2, equal):
+    assert numbers(Num1, Num2) == equal
+
+## 4
 @pytest.mark.parametrize("word, answer", [("racecar", True), ("543212345", True), ("randomword", False), (12, False)])
 def test_isPalindrome(word, answer):
     assert isPalindrome(word) == answer
 
 
 ## these arent exactly working...
-def geninputs():
-    inputs = ["64", "8", "81", "9"]
+@pytest.mark.parametrize("first, second, equal", [(64, 8, 8), (81, 9, 9)])
+def geninputs(first=[64,81], second=[8,9]):
+    inputs = [first, second]
 
     for item in inputs:
         yield item
 
+
 GEN = geninputs()
 
-def test_divide(divider):
-    divider.setattr('builtins.input', lambda _: next(GEN))
 
-    assert divide()
+##5
+def test_divide(monkeypatch, equal=[8,9]):
+    monkeypatch.setattr(('builtins.input', lambda _: next(GEN)))
+    ##divider.setattr('builtins.input', lambda _: next(GEN))
+
+    assert divide() == equal
